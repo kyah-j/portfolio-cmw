634eaf08f1a24c1b4c840825639f105fdedb9ac8
kyah-j <kyannahjeanne@gmail.com>
2026-02-25 16:49:07 +0100

    rajout fichier

---
+++ langue.js
@@ -0,0 +1,29 @@
+var textesEnFrancais = {
+"titre1" : "Mon Portfolio - Kyannah JEANNE",
+"titre2" : "Présentation",
+"p1" : "Je m'appelle Kyannah, rédactrice web SEO"
+};
+
+var textsInEnglish = {
+"titre1" : "My Portofolio - Kyannah JEANNE",
+"titre2" : "Presentation",
+"p" : " I'm Kyannah, SEO web editor"
+};
+
+function changeLanguage(langue){
+    var textes = textsInEnglish
+    if (langue == "français"){
+        textes = textesEnFrancais
+    }
+    Object.keys(textes).forEach(function(cle){
+        document.querySelector('#'+cle).innerHTML = textes[cle]})
+};
+
+document.querySelectorAll("input").forEach(function(input) {
+    input.addEventListener("click", function (e) {
+        changeLanguage(e.srcElement.value);
+    });
+});
+
+
+
